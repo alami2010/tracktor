680eac7defc23a7e2975b13546b8b3bc6d9e4a5a
ydahar <youssef.dahar-ext@fnacdarty.com>
2020-02-24 17:48:16 +0100

    up

---
+++ src/main/java/com/api/prices/crypto/cryptoprices/client/alphavantage/currencies/CryptoCurrency.java
@@ -0,0 +1,42 @@
+package com.api.prices.crypto.cryptoprices.client.alphavantage.currencies;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Representation of the data that is returned by the CryptoCurrencies endpoints
+ * in the API.
+ * The values that are returned for the adjusted CryptoCurrencies endpoints are
+ * {@code Optional} typed.
+ */
+@Data
+public class CryptoCurrency {
+
+    public CryptoCurrency(double open, double high, double low, double close, double volume, Double marketCup, Date date) {
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
+        this.marketCup = marketCup;
+        this.date = date;
+    }
+
+    private double open;
+
+    private double high;
+
+    private double low;
+
+    private double close;
+
+    private double volume;
+
+    private Double marketCup = null;
+
+    private Date  date;
+
+}
--- src/main/java/com/api/prices/crypto/cryptoprices/service/IndicatorTechnicalService.java
+++ src/main/java/com/api/prices/crypto/cryptoprices/service/IndicatorTechnicalService.java
@@ -1,16 +1,9 @@
 package com.api.prices.crypto.cryptoprices.service;
 
-import com.api.prices.crypto.cryptoprices.client.CoinMarketPlaceClient;
 import com.api.prices.crypto.cryptoprices.client.alphavantage.configuration.IAlphaVantageClient;
 import com.api.prices.crypto.cryptoprices.client.alphavantage.currencies.*;
 import com.api.prices.crypto.cryptoprices.client.alphavantage.request.OutputSize;
 import com.api.prices.crypto.cryptoprices.client.alphavantage.timeseries.MissingRequiredQueryParameterException;
-import com.api.prices.crypto.cryptoprices.client.pojo.Currency;
-import com.api.prices.crypto.cryptoprices.client.pojo.CurrencyInformation;
-import com.api.prices.crypto.cryptoprices.client.pojo.CurrencyInformationStats;
-import com.api.prices.crypto.cryptoprices.entity.BinanceCurrency;
-import com.api.prices.crypto.cryptoprices.entity.CurrencyToTrack;
-import com.api.prices.crypto.cryptoprices.entity.TableHtml;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,16 +12,15 @@ import org.ta4j.core.BaseTimeSeries;
 import org.ta4j.core.TimeSeries;
 import org.ta4j.core.indicators.SMAIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
-import org.ta4j.core.num.Num;
 
 import java.io.IOException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 @Service
 public class IndicatorTechnicalService {
@@ -36,8 +28,6 @@ public class IndicatorTechnicalService {
 
     AlphaVantageCurrency[] alphaVantageCurrencies = AlphaVantageCurrency.values();
     int indexCurrentCurrency = 0;
-
-    List<String> enumA = Arrays.stream(alphaVantageCurrencies).map(Enum::name).collect(Collectors.toList());
 
 
     @Autowired
@@ -50,18 +40,26 @@ public class IndicatorTechnicalService {
 
 
         try {
-            CryptoCurrenciesResult btc = alphaVantageClient.getCryptoCurrencies(CryptoCurrenciesFunction.DIGITAL_CURRENCY_DAILY, Market.USD, currency, OutputSize.COMPACT);
-            System.out.println(btc);
-            double somme = 0;
+            CryptoCurrenciesResult cryptoCurrenciesResult = alphaVantageClient.getCryptoCurrencies(CryptoCurrenciesFunction.DIGITAL_CURRENCY_DAILY, Market.USD, currency, OutputSize.COMPACT);
+
+            List<CryptoCurrency> cryptoCurrencies = mapCryptoCurrenciesResult(cryptoCurrenciesResult);
+
+            System.out.println(cryptoCurrenciesResult);
             TimeSeries series = new BaseTimeSeries.SeriesBuilder().withName("AXP_Stock").build();
-            ;
+
 
 
             Date startDate = Date.from(ZonedDateTime.now().minusMonths(1).toInstant());
 
-            btc.getCryptoCurrencies().entrySet().stream().
-                    filter(cryptoCurrenciesEntry -> cryptoCurrenciesEntry.getKey().after(startDate)).
-                    forEach(cryptoCurrenciesEntry -> {
+            Stream<CryptoCurrency> cryptoCurrencyStream = cryptoCurrencies.stream().
+                    filter(cryptoCurrenciesEntry -> cryptoCurrenciesEntry.getDate().after(startDate));
+
+
+
+            Supplier< Stream<CryptoCurrency>> cryptoCurrencySupplier = () ->cryptoCurrencyStream;
+
+
+            forEach(cryptoCurrenciesEntry -> {
 
                         Date dateHistorian = cryptoCurrenciesEntry.getKey();
                         CryptoCurrencies cryptoCurrencyHisotrian = cryptoCurrenciesEntry.getValue();
@@ -85,20 +83,23 @@ public class IndicatorTechnicalService {
 // Getting a longer SMA (e.g. over the 30 last ticks)
             SMAIndicator longSma = new SMAIndicator(closePrice, 2);
             System.out.println("30-ticks-SMA value at the 42nd index: " + longSma.getValue(28));
-            System.out.println(somme / 31);
         } catch (Exception e) {
-            System.out.println("ERROR " + currency +" "+ e.getMessage());
+            System.out.println("ERROR " + currency + " " + e.getMessage());
 
-            enumA.remove(currency);
-            System.out.println(enumA);
         }
 
 
-        // String makeAPICall = coinMarketPlaceClient.makeAPICall(CoinMarketPlaceClient.UrlManager.TEST, null, null);
-        System.out.println("hhelooo");
+    }
 
+    private List<CryptoCurrency> mapCryptoCurrenciesResult(CryptoCurrenciesResult cryptoCurrenciesResult) {
+        List<CryptoCurrency> cryptoCurrencies = new ArrayList<>();
+        cryptoCurrenciesResult.getCryptoCurrencies().forEach( (date, cryptoCur) ->{
 
+            CryptoCurrency cryptoCurrency = new CryptoCurrency(cryptoCur.getOpen(),cryptoCur.getHigh(),cryptoCur.getLow(),cryptoCur.getClose(),cryptoCur.getVolume(),cryptoCur.getMarketCup(),date);
+            cryptoCurrencies.add(cryptoCurrency);
 
+        } );
+        return cryptoCurrencies;
     }
 
     private String getCurrentCurrency() {
